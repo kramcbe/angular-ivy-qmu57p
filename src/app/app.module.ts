9a7e85a93712771d18933bcf7d4ee3af20eec133
kramcbe <kumarstampsweb@gmail.com>
2021-11-30 22:11:11 +0530

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,16 @@ import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, HeroesComponent],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
